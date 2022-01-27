5d8df5a84c69315c04d334ac5c314d524d5a9c34
dengshulei <dengshulei@126.com>
2022-01-27 18:52:33 +0800

    完成了Bind和Register

---
+++ binder.go
@@ -0,0 +1,53 @@
+package gin_auto_router
+
+import (
+	"github.com/gin-gonic/gin"
+	"reflect"
+	"strings"
+)
+
+var paths = make(map[int]string)
+
+//绑定基本路由
+func Bind(e *gin.Engine) {
+	pathInit()
+	for _, path := range paths {
+		e.POST(path, match(path))
+	}
+}
+
+//绑定路由组
+func BindGroup(r *gin.RouterGroup) {
+	pathInit()
+	for _, path := range paths {
+		r.POST(path, match(path))
+	}
+}
+
+//组装path
+func pathInit() {
+	i := 0
+	for class, value := range Routes {
+		for method, _ := range value {
+			path := "/" + class + "/" + method
+			paths[i] = path
+			i += 1
+		}
+	}
+}
+
+//根据path匹配对应的方法
+func match(path string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fields := strings.Split(path, "/")
+		if len(fields) < 3 {
+			return
+		}
+		v, ok := Routes[fields[1]][fields[2]]
+		if ok  {
+			arguments := make([]reflect.Value, 1)
+			arguments[0] = reflect.ValueOf(c) // *gin.Context
+			v.Method.Call(arguments)
+		}
+	}
+}
---
+++ register.go
@@ -0,0 +1,46 @@
+package gin_auto_router
+
+import (
+	"reflect"
+	"strings"
+)
+type Route struct {
+	Method reflect.Value
+	Args   []reflect.Type
+}
+
+var Routes = make(map[string]map[string]Route)
+
+//注册路由方法
+func Register(controller interface{}) bool {
+	v := reflect.ValueOf(controller)
+	//非控制器或无方法则直接返回
+	if v.NumMethod() 	== 0 {
+		return false
+	}
+
+	//取得控制器名称
+	tmp := reflect.TypeOf(controller).String()
+	module := tmp
+	if strings.Contains(tmp, ".") {
+		module = tmp[strings.Index(tmp, ".") + 1:]
+	}
+
+	//遍历方法
+	for i:= 0; i < v.NumMethod(); i++ {
+		method := v.Method(i)
+		action := v.Type().Method(i).Name
+
+		//遍历参数
+		params := make([]reflect.Type, 0, v.NumMethod())
+		for j := 0; j < method.Type().NumIn(); j++ {
+			params = append(params, method.Type().In(j))
+		}
+
+		if Routes[module] == nil {
+			Routes[module] = make(map[string]Route)
+		}
+		Routes[module][action] = Route{method,params}
+	}
+	return true
+}
