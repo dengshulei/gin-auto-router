98f728785f46f6eee76c30dc1ea7d5d686b1815a
dengshulei <dengshulei@126.com>
2023-06-28 19:26:57 +0800

    fix (core): 优化注册和绑定的逻辑

--- binder.go
+++ binder.go
@@ -3,51 +3,35 @@ package gin_auto_router
 import (
 	"github.com/gin-gonic/gin"
 	"reflect"
-	"strings"
 )
-
-var paths = make(map[int]string)
 
 // Bind 绑定基本路由
 func Bind(e *gin.Engine) {
-	pathInit()
-	for _, path := range paths {
-		e.POST(path, match(path))
-	}
+	r := e.Group("/")
+	baseBind(r)
 }
 
 // BindGroup 绑定路由组
 func BindGroup(r *gin.RouterGroup) {
-	pathInit()
-	for _, path := range paths {
-		r.POST(path, match(path))
-	}
+	baseBind(r)
 }
 
-// pathInit 组装path
-func pathInit() {
-	i := 0
+// baseBind 执行路由绑定
+func baseBind(r *gin.RouterGroup) {
 	for class, value := range Routes {
-		for method := range value {
+		for method, v := range value {
 			path := "/" + class + "/" + method
-			paths[i] = path
-			i += 1
+
+			r.POST(path, HandlerFunc(v))
 		}
 	}
 }
 
-// match 根据path匹配对应的方法
-func match(path string) gin.HandlerFunc {
+// HandlerFunc 将控制器方法转为 gin.HandlerFunc 方法
+func HandlerFunc(v Route) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		fields := strings.Split(path, "/")
-		if len(fields) < 3 {
-			return
-		}
-		v, ok := Routes[fields[1]][fields[2]]
-		if ok {
-			arguments := make([]reflect.Value, 1)
-			arguments[0] = reflect.ValueOf(c) // *gin.Context
-			v.Method.Call(arguments)
-		}
+		arguments := make([]reflect.Value, 1)
+		arguments[0] = reflect.ValueOf(c)
+		v.Method.Call(arguments)
 	}
 }
